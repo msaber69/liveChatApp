9100ea01e6d14452d37488b5d8c478f5d403d57f
Mohammed saber Bellaaziri <mohammedsaberbellaaziri@Sabers-MacBook-Air.local>
2024-10-29 18:23:13 +0100

    Initial commit

---
+++ src/main/java/com/livechat/app/LiveChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.livechat.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveChatAppApplication.class, args);
+	}
+
+}
